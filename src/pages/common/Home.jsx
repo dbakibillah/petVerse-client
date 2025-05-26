fa848659213ac7e70daf7006062f0dfa54d0492b
tushar123 <asiif32ahamed@gmail.com>
2025-05-27 01:34:14 +0600

    petcarehub

--- src/pages/common/Home.jsx
+++ src/pages/common/Home.jsx
@@ -4,15 +4,15 @@ import PetCareHub from "../../components/home/PetCareHub";
 import OfferNav from "../../components/home/OfferNav";
 import Newsletter from "../../components/home/Newsletter";
 const Home = () => {
-    return (
-        <section className="">
-            <Helmet title="Petverse | Home" />
-            <OfferNav />
-            <AboutUsSec />
-                     <PetCareHub />
-                       <Newsletter />
-        </section>
-    );
+  return (
+    <section className="">
+      <Helmet title="Petverse | Home" />
+      <OfferNav />
+      <AboutUsSec />
+      <PetCareHub />
+      <Newsletter />
+    </section>
+  );
 };
 
 export default Home;
